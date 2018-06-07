0ff6d5b4a54ca22211543038b26eff95bb8c5c54
Carlosaugustob <38558653+Carlosaugustob@users.noreply.github.com>
2018-06-07 11:29:12 -0300

    continuar em casa
    casa

--- ListaArrayList/src/Exercicio2.java
+++ ListaArrayList/src/Exercicio2.java
@@ -19,15 +19,15 @@ public class Exercicio2 {
     
       
             
-            nomes.add("cab");
-            nomes.add("calanguinho pipinha"); 
-            nomes.add("mano mendigo");
-            nomes.add("poderoso castiga");
-            nomes.add("vin diesel");
-            nomes.add("vin gasolina");
-            nomes.add("saca rolha");
-            nomes.add("chupa cabra");
-            nomes.add("asa de morcego");
+            nomes.add("Cab");
+            nomes.add("Calanguinho pipinha"); 
+            nomes.add("Mano mendigo");
+            nomes.add("Poderoso castiga");
+            nomes.add("Vin diesel");
+            nomes.add("Vin gasolina");
+            nomes.add("Saca rolha");
+            nomes.add("Chupa cabra");
+            nomes.add("Asa de morcego");
         
  JOptionPane.showMessageDialog(null, nomes.get(0)+ "." + "\n" + nomes.get(1)+ "." + "\n" + nomes.get(2)+ "." +
             "\n" + nomes.get(3)+ "." + "\n" + nomes.get(4)+ "." + "\n" + nomes.get(5)+ "." + "\n" + 
